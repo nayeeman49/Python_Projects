bd2e3f11995bb61b0a72d7e37f396d1791e1873f
Nayeem An-noor <nayeeman49@googlemail.com>
2024-12-18 23:20:57 +0000

    Create Variables.py

---
+++ Variables.py
@@ -0,0 +1,17 @@
+# A variable is created the moment you first assign a value to it.
+x = 4       # x is of type int
+x = "Sally" # x is now of type str
+print(x)
+
+print()
+
+#If you want to specify the data type of a variable, this can be done with casting.
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0
+
+# You can get the data type of a variable with the type() function.
+x = 5
+y = "John"
+print(type(x))
+print(type(y))
